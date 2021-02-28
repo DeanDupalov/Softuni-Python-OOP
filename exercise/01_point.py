307a7e4b6385d188941d410906c55a7657095b0d
Dean Dupalov <75751527+DeanDupalov@users.noreply.github.com>
2021-02-28 15:13:37 +0000

    exercise

---
+++ exercise/01_point.py
@@ -0,0 +1,19 @@
+import math
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_x(self, new_x):
+        self.x = new_x
+
+    def set_y(self, new_y):
+        self.y = new_y
+
+    def distance(self, x, y):
+        return math.sqrt((self.x - x) ** 2 + (self.y - y) ** 2)
+
+
+
